5f5af10e7d02d912eec1e559f300470f4cef2304
Altairjrgs00 <joser.gambarinis@gmail.com>
2026-09-03 00:25:08 -0500

    Creada la clase base Persona con atributos protegidos

--- Java/Persona.java
+++ Java/Persona.java
@@ -1,9 +1,28 @@
 package Java;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("==================================================");
-        System.out.println("¡SGA-DO: ENTORNO CONFIGURADO CON ÉXITO!");
-        System.out.println("==================================================");
+public class Persona {
+    // Seran loa atributos o variables
+    protected String Cedula;
+    protected String Nombre;
+    protected String Correo;
+
+    // Ahora los constructores
+    public Persona(String cedula, String nombre, String correo) {
+        this.Cedula = cedula;
+        this.Nombre = nombre;
+        this.Correo = correo;
+    }
+
+    //Establecemos las funciones get para leer los datos desde afuera
+    public String getCedula() {
+        return this.Cedula;
+    }
+
+    public String getNombre() {
+        return this.Nombre;
+    }
+
+    public String getCorreo() {
+        return this.Correo;
     }
 }
