29b600ab0a72d3311179ead8b84cc3997496698b
Altairjrgs00 <joser.gambarinis@gmail.com>
2026-09-06 00:44:34 -0500

    Menú interactivo creado y asegurado contra fallos

--- Java/Main.java
+++ Java/Main.java
@@ -1,34 +1,79 @@
 package Java;
 
+import java.util.Scanner; 
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("==================================================");
-        System.out.println("----- PRUEBA DE REGLAS EN JAVA -----");
-        System.out.println("==================================================");
-   
-        //Caso 1: Wade Wilson debe aprobar un curso con promedio de 10   
-        Alumno alumnoCurso = new Alumno("V-101", "Wade Wilson", "wade.wilson@ejemplo.com", "Curso"); 
-        alumnoCurso.agregarNota(10.0);
-        alumnoCurso.agregarNota(10.0);
-        alumnoCurso.agregarNota(10.0);
+        public static void main(String[] args) {
+        
+        //Necesitamos una herramienta para leer la entrada del usuario, por lo que importamos la clase Scanner de java.util.
+        
+        Scanner teclado = new Scanner(System.in);
+        int opcion = 0;
 
-        System.out.println("Alumno: " + alumnoCurso.getNombre() + " | Programa: " + alumnoCurso.getTipoPrograma());
-        System.out.println("Promedio: " + alumnoCurso.calcularPromedio() + " | Estatus: " + alumnoCurso.chequearEstatus());
-    
-    
-        System.out.println("==================================================");
-    
-        // Caso 2: Peter Parker debe aprobar un Bootcamp pero tiene una nota de 13)
-        // La regla estricta exige aplazarlo automáticamente por tener una nota menor a 14
-        Alumno alumnoBootcamp = new Alumno("V-202", "Peter Parker", "peter.parker@ejemplo.com", "Bootcamp");
-        alumnoBootcamp.agregarNota(20.0);
-        alumnoBootcamp.agregarNota(20.0);
-        alumnoBootcamp.agregarNota(13.0); 
+        //Creamos un bucle continuo ¨While¨ que se ejecutará hasta que el usuario decida salir del programa.
+        while (opcion != 7) {
+            System.out.println("==================================================================");
+            System.out.println("============== SGA:DO - SISTEMA DE DIPLOMADOSONLINE ==============");
+            System.out.println("==================================================================");
+            System.out.println("1. Registrar Alumno");
+            System.out.println("2. Registrar Profesor");
+            System.out.println("3. Registrar notas a un alumno");
+            System.out.println("4. Deshacer ultimo registro de nota");
+            System.out.println("5. Generar cola de certificados");
+            System.out.println("6. Mostrar Reporte General ");
+            System.out.println("7. Salir del Sistema");
+            System.out.println("==================================================================");
+            System.out.print("Ingrese una opción: ");
+        
+            //Creamos un salvavidas para evitar que el programa se caiga si el usuario ingresa un valor no numérico.
+            try {
+                //hacemos que el programa lea la opción ingresada por el usuario y la lea como un numero
+                opcion = Integer.parseInt(teclado.nextLine());
 
-        System.out.println("Alumno: " + alumnoBootcamp.getNombre() + " | Programa: " + alumnoBootcamp.getTipoPrograma());
-        System.out.println("Promedio: " + alumnoBootcamp.calcularPromedio() + " | Estatus: " + alumnoBootcamp.chequearEstatus());
-        System.out.println("==================================================");
+                //hacemos un switch para que el programa ejecute la opción ingresada por el usuario.
+                switch (opcion) {
 
+                   case 1: 
+                        System.out.println("\nRegistrar alumno\n");
+                        break;
+                    case 2:
+                        System.out.println("\nRegistrar profesor\n");
+                        break;
+                    case 3:
+                        System.out.println("\nRegistrar notas a un alumno\n");
+                        break;
+                    case 4:
+                        System.out.println("\nDeshacer ultimo registro de nota\n");
+                        break;
+                    case 5:
+                        System.out.println("\nGenerar cola de certificados\n");
+                        break;
+                    case 6:
+                        System.out.println("\nMostrar Reporte General\n");
+                        break;
+                    case 7:
+                        System.out.println("\nSaliendo del programa...\n");
+                        break;
+                    default:
+                        System.out.println("\nError: Opción inválida, ingrese un número del 1 al 7.\n");
+                        System.out.println("Presione Enter para continuar...");
+                        teclado.nextLine();
+                        break;
+               
+                    }
+
+                } catch (NumberFormatException e) {
+                // Si el usuario escribe una letra o deja vacio, Java atrapa el error aqui
+                System.out.println("Error: Ingrese un valor numérico válido (1-7)");
+                System.out.println("Presione Enter para continuar...");
+                teclado.nextLine(); // Esperamos a que el usuario presione Enter antes de continuar
+                opcion = 0; // Reseteamos la opcion para que el bucle continue normal
+            }
+        }
+        // Cerramos el lector de teclado al finalizar para liberar memoria
+        teclado.close();
     }
 
 }
+
+    
