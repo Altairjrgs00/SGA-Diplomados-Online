e0cd0a61cc88c795e53d81ad7fe662c8883558ba
Altairjrgs00 <joser.gambarinis@gmail.com>
2026-09-13 03:06:41 -0500

    Código Java Corregido y Probado

--- Java/GestorAcademico.java
+++ Java/GestorAcademico.java
@@ -204,6 +204,6 @@ public class GestorAcademico {
             System.out.println("Error al exportar certificados: " + e.getMessage());
         }
         
-        System.out.println("===================================================================");
+        System.out.println("==================================================================");
     }
 }    
